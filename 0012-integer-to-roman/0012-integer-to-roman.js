7c55e2d23f7ec1058cc2a8832c4ff6fdf5906b1a
JERRIPOTHULA V CHAITANYA SAI <chaitanyajerripothula95@gmail.com>
2023-06-24 09:59:29 +0530

    Time: 129 ms (50.58%), Space: 50.3 MB (18.08%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+  const decimalToRoman = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M'
+  };
+
+  let result = '';
+
+  const decimals = Object.keys(decimalToRoman).sort((a, b) => a - b);
+
+  for (let i = decimals.length - 1; i >= 0; i--) {
+    const decimal = decimals[i];
+    while (num >= decimal) {
+      num -= decimal;
+      result += decimalToRoman[decimal];
+    }
+  }
+
+  return result;
+};
