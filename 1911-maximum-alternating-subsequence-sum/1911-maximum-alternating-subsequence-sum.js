caf783b49a6032b6d4d1b48e5b324a6556e1f84a
JERRIPOTHULA V CHAITANYA SAI <chaitanyajerripothula95@gmail.com>
2023-06-26 03:16:15 +0530

    Time: 81 ms (97.37%), Space: 53.5 MB (100.00%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAlternatingSum = function(nums) {
+     let evenSum = nums[0];
+  let oddSum = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    evenSum = Math.max(evenSum, nums[i] + oddSum);
+    oddSum = Math.max(oddSum, evenSum - nums[i]);
+  }
+
+  return Math.max(evenSum, oddSum);
+    
+};
