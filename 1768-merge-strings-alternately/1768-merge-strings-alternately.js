3e37c7dff5029f0ef6403b2d87814acb563822a6
JERRIPOTHULA V CHAITANYA SAI <chaitanyajerripothula95@gmail.com>
2023-06-26 06:12:15 +0530

    Time: 65 ms (27.89%), Space: 42.3 MB (49.40%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,17 +4,17 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-   let merged = '';
+   let merged = [];
   const maxLength = Math.max(word1.length, word2.length);
   
   for (let i = 0; i < maxLength; i++) {
     if (word1[i]) {
-      merged += word1[i];
+      merged.push(word1[i])
     }
     if (word2[i]) {
-      merged += word2[i];
+      merged.push(word2[i]);
     }
   }
   
-  return merged; 
+  return merged.join('');
 };
