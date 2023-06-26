0f0e4cb528dc9165ee926686e538c2afe365bbef
JERRIPOTHULA V CHAITANYA SAI <chaitanyajerripothula95@gmail.com>
2023-06-26 05:39:12 +0530

    Time: 46 ms (98.77%), Space: 42.5 MB (5.23%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.js
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -1,33 +1,45 @@
+
 var TimeLimitedCache = function() {
-  this.cache = {};
+    this.cache = {};
 };
 
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
 TimeLimitedCache.prototype.set = function(key, value, duration) {
-  if (this.cache[key]) {
-    // Key already exists, update value and duration
-    this.cache[key].value = value;
-    this.cache[key].expirationTime = Date.now() + duration;
-    return true; // Key already existed
-  } else {
-    // Key doesn't exist, add new entry
-    this.cache[key] = {
-      value: value,
-      expirationTime: Date.now() + duration
-    };
-    return false; // Key didn't exist
-  }
+    if(this.cache[key]) {
+        this.cache[key].value = value;
+        this.cache[key].expirationTime = Date.now() + duration;
+        return true;
+    } else {
+        this.cache[key] = {
+            value: value,
+            expirationTime: Date.now() + duration
+        };
+        return false;
+    }
 };
 
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
 TimeLimitedCache.prototype.get = function(key) {
-  if (this.cache[key] && this.cache[key].expirationTime > Date.now()) {
-    return this.cache[key].value; // Return value if key is unexpired
-  } else {
-    return -1; // Return -1 if key doesn't exist or has expired
-  }
+    if(this.cache[key] && this.cache[key].expirationTime > Date.now()) {
+        return this.cache[key].value;
+    } else {
+        return -1;
+    }
 };
 
+/** 
+ * @return {number} count of non-expired keys
+ */
 TimeLimitedCache.prototype.count = function() {
-  let count = 0;
+     let count = 0;
   const currentTime = Date.now();
   for (let key in this.cache) {
     if (this.cache[key].expirationTime > currentTime) {
@@ -36,3 +48,11 @@ TimeLimitedCache.prototype.count = function() {
   }
   return count;
 };
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
